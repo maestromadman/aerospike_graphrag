6c34da9d54d7b2ab54c6621a69d1021593bb05ab
maestromadman <amallya0523@gmail.com>
2024-05-13 14:59:42 -0700

    mixtral

--- mixtral_8x7b.py
+++ mixtral_8x7b.py
@@ -1,17 +1,21 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+#8-bit model (originally float16)
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 
+quantization_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_compute_dtype=torch.bfloat16)
 model_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForCausalLM.from_pretrained(model_id, device_map="auto")
 
-user_input = input()
+model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=quantization_config, device_map="auto")
 
+text = "Hello my name is"
 messages = [
-    {"role": "user", "content": user_input}
-    #{"role": "assistant", "content": "Well, I'm quite partial to a good squeeze of fresh lemon juice. It adds just the right amount of zesty flavour to whatever I'm cooking up in the kitchen!"},
-    #{"role": "user", "content": "Do you have mayonnaise recipes?"}
+    {"role": "user", "content": "What is your favourite condiment?"},
+    {"role": "assistant", "content": "Well, I'm quite partial to a good squeeze of fresh lemon juice. It adds just the right amount of zesty flavour to whatever I'm cooking up in the kitchen!"},
+    {"role": "user", "content": "Do you have mayonnaise recipes?"}
 ]
 
-inputs = tokenizer.apply_chat_template(messages, return_tensors="pt").to("cuda")
-outputs = model.generate(inputs, max_new_tokens=100)
+input_ids = tokenizer.apply_chat_template(messages, return_tensors="pt").to("cuda")
+
+outputs = model.generate(input_ids, max_new_tokens=20)
 print(tokenizer.decode(outputs[0], skip_special_tokens=True))
