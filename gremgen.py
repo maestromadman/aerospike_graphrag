279740c98045e9bb5004c7248535a9192669c38f
Ajay Mallya <amallya0523@gmail.com>
2024-05-15 12:49:06 -0700

    gremgen

--- gremgen.py
+++ gremgen.py
@@ -2,36 +2,33 @@ from gremlin_python.process.anonymous_traversal import traversal
 from gremlin_python.process.traversal import IO
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
 
-if __name__ == '__main__':
-    # Create GraphTraversalSource to remote server.                                                                                                
-    g = traversal().with_remote(DriverRemoteConnection('ws://localhost:8182/gremlin', 'g'))
+# Create GraphTraversalSource to remote server.                                                                                                
+g = traversal().with_remote(DriverRemoteConnection('ws://localhost:8182/gremlin', 'g'))
 
  # Sample queries with the air-routes data set. To use these queries, download
     # the data set and load it with the following:
     # g.with_("evaluationTimeout", 24 * 60 * 60 * 1000).\
-    #   io("/opt/aerospike-graph/data/air-routes-small.graphml").\
+    #   io("/opt/aerospike-graph/data/air-routes-latest.graphml").\
     #   with_(IO.reader, IO.graphml).\
     #   read().iterate()                                                                                              
+
+summary = g.call("summary").next()
+
+edges = {k: v for k, v in summary.items() if k in {'Edge properties by label'}} # <type 'dict'>
+edge_props = edges.get('Edge properties by label', {}).get('route', {})
+edge_props_str = " \n".join(edge_props)
+
+vertex_props = summary.get('Vertex properties by label', {}).get('airport', {}) # <type 'dict'>
+vertex_props_str = " ".join(vertex_props)
     
-    summary = g.call("summary").next()
-
-    edges = {k: v for k, v in summary.items() if k in {'Edge properties by label'}} # <type 'dict'>
-    edge_props = edges.get('Edge properties by label', {}).get('route', {})
-    edge_props_str = " \n".join(edge_props)
-
-    vertex_props = summary.get('Vertex properties by label', {}).get('airport', {}) # <type 'dict'>
-    vertex_props_str = " ".join(vertex_props)
-    
-
-    sample_queries = "Find the number of airports in this graph: g.V().has('code','DFW')\n Find the number of flights going out of the airport SFO: g.V().hasLabel('airport').count()\n Get all cities with flights that are >4000 miles: g.V().has('code','SFO').outE().count()\n Find all the airports in the USA you can fly to from London Heathrow (LHR): g.V().has('dist', P.gt(4000L)).inV().values('city').dedup()\n g.V().has('code','LHR').out('route').has('country','US').values('code')\n Find all the unique locations in the world and in the US that I can get to from SFO through a 2 hop flight: g.V().has('code', 'SFO').out().out().dedup().fold().project('totalAirportCountFromSFO', 'USAirportCountFromSFO').by(__.unfold().count()).by(__.unfold().has('country', 'US').count())"
-    schema_context = '''\
+sample_queries = "Find the number of airports in this graph: g.V().has('code','DFW')\n Find the number of flights going out of the airport SFO: g.V().hasLabel('airport').count()\n Get all cities with flights that are >4000 miles: g.V().has('code','SFO').outE().count()\n Find all the airports in the USA you can fly to from London Heathrow (LHR): g.V().has('dist', P.gt(4000L)).inV().values('city').dedup()\n g.V().has('code','LHR').out('route').has('country','US').values('code')\n Find all the unique locations in the world and in the US that I can get to from SFO through a 2 hop flight: g.V().has('code', 'SFO').out().out().dedup().fold().project('totalAirportCountFromSFO', 'USAirportCountFromSFO').by(__.unfold().count()).by(__.unfold().has('country', 'US').count())"
+schema_context = '''\
         Here are some example questions and Gremlin queries that traverse a graph to find their answers: {sample_queries}.
         
         Insert ONLY THESE edge properties ({edge_props}) and THESE vertex properties ({vertex_props}) into a Gremlin query help find the answer to my question.
         
         Question: {question}
         '''
-    
 
 from llama_cpp import Llama
 
@@ -63,5 +60,5 @@ prompt = schema_context.format(edge_props=edge_props_str, vertex_props=vertex_pr
 res1 = llm(prompt, **generation_kwargs)  # Res is a dictionary
 
 # Print the generated text
-print(res1["choices"][0]["text"])  # res is short for result
+res1_out = print(res1["choices"][0]["text"])  # res is short for result
 
