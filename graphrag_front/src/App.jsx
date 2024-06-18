b401873e7e0772cc006488f1d61b9b7fa56ff9c6
Ajay Mallya <amallya0523@gmail.com>
2024-06-18 08:40:16 -0700

    basic search bar

--- graphrag_front/src/App.jsx
+++ graphrag_front/src/App.jsx
@@ -1,27 +1,14 @@
-import { useState, useEffect } from 'react';
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css';
-
+import React from 'react';
 
 const App = () => {
 
-  const [counter, setCounter] = useState(0);
-
-  // [] represents "dependencies" that dictate if the effect should be run again
-  useEffect(() => {
-    alert("You've changed the counter to " + counter)
-  }, [counter]);
-
-  return (
-    <> 
-      <div className= "App">  
-        <button onClick={() => setCounter((prevCount) => prevCount -1)}>-</button>
-        <h1>{counter}</h1>
-        <button onClick={() => setCounter((prevCount) => prevCount +1)}>+</button>
-      </div>
-    </>
-  )
+    return (
+        <div className = "App"> 
+          <h1>Aerospike Airlines</h1>  
+          <input placeholder='Search'/>
+        </div>
+    
+    );
 }
 
-export default App
+export default App;
